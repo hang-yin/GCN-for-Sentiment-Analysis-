169620ac3cbaa80a986a515a6838c8404c2d2f66
Joey Yang <zeyuyang2022@u.northwestern.edu>
2022-05-20 19:29:47 -0500

    first commit

---
+++ dataLoader.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+import numpy as np
+
+def sampleFunction(inputFeature):
+    """
+    This is a description of the function
+    
+    Args:
+        inputFeature - (np.ndarray) This is what the feature is
+    Returns:
+        result - (int) This is what is returned
+    """
+
+    result = 55
+    return result
