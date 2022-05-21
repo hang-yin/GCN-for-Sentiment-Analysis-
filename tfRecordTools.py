90aab7310e559801dbec092bb4c3e6a10cc1799f
Joey Yang <zeyuyang2022@u.northwestern.edu>
2022-05-20 19:47:34 -0500

    other notebook

---
+++ tfRecordTools.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+
+
+def _int64_feature(value):
+    """
+    Returns int64 tf.train.Feature.
+
+    Args:
+        value - (np.ndarray) array of ints
+    """
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=value.tolist()))
+
+
+def _bytes_feature(value):
+    """
+    Returns bytes tf.train.Feature.
+
+    Args:
+        value - (string) string
+    """
+    return tf.train.Feature(
+        bytes_list=tf.train.BytesList(value=[value.encode('utf-8')]))
+
+
+def _float_feature(value):
+    """
+    Returns float tf.train.Feature.
+
+    Args:
+        value - (np.ndarray) array of floats
+
+    """
+    return tf.train.Feature(float_list=tf.train.FloatList(value=value.tolist()))
